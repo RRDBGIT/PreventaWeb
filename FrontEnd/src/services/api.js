967a5e65f4afdb5386e89e4ad6bbc540bc029d20
RRDBGIT <brizuelaruben@hotmail.com>
2025-12-12 12:39:50 -0300

    Commit automÃ¡tico: aplicar .gitignore y limpiar archivos grandes

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 // ✅ baseURL incluye /api → las rutas en los componentes NO deben llevar /api
-const API = axios.create({    
-    baseURL: 'http://192.168.1.101:5000/api',
-    //baseURL: 'https://preventaweb-api.onrender.com/api', 
+const API = axios.create({ 
+    baseURL: 'https://preventaweb-api.onrender.com/api', 
     timeout: 10000,
 });
 
